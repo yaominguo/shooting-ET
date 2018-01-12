6a2e8a54f11959850a133fa57fa1a158b963f1a3
yaominguo <missgmy@yahoo.com>
2018-01-12 17:40:45 +0800

    重构

--- src/game_functions.py
+++ src/game_functions.py
@@ -3,21 +3,31 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, rocket):
+    """响应按键按下"""
+    if event.key == pygame.K_RIGHT:
+        rocket.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        rocket.moving_left = True
+
+
+def check_keyup_events(event, rocket):
+    """响应按键松开"""
+    if event.key == pygame.K_RIGHT:
+        rocket.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        rocket.moving_left = False
+
+
 def check_events(rocket):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                rocket.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                rocket.moving_left = True
+            check_keydown_events(event, rocket)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                rocket.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                rocket.moving_left = False
+            check_keyup_events(event, rocket)
 
 
 def update_screen(ai_settings, screen, rocket):
