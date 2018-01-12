c9cc01239ab0e87141b9099dfe0d47c0dba3bdd7
yaominguo <missgmy@yahoo.com>
2018-01-12 15:52:08 +0800

    初始化设置

---
+++ src/setting.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+class Settings():
+    """存储设置选项"""
+
+    def __init__(self):  # 初始化设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
